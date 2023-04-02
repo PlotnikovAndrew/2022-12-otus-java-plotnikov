bc8463d0f7792710fb97805222027d2907585e00
PlotnikovAndrew <drunyy@gmail.com>
2023-04-02 22:57:55 +0300

    wip_1

--- hw16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,5 +1,8 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
@@ -10,5 +13,22 @@ public class FileSerializer implements Serializer {
     @Override
     public void serialize(Map<String, Double> data) {
         //формирует результирующий json и сохраняет его в файл
+        var gson = new Gson();
+
+        String json = gson.toJson(data);
+        System.out.println(json);
+        System.out.println(json);
+    }
+
+    public static void main(String[] args){
+//        var inputDataFileName = "inputData.json";
+        var inputDataFileName = "inputData.json";
+        Map<String, Double> inputDataFile = new HashMap<>();
+        inputDataFile.put("val1", 0.0);
+        inputDataFile.put("val2", 1.0);
+        inputDataFile.put("val3", 2.0);
+        FileSerializer fileSerializer = new FileSerializer(inputDataFileName);
+        fileSerializer.serialize(inputDataFile);
+
     }
 }
--- hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,39 @@
 package ru.otus.dataprocessor;
 
 import ru.otus.model.Measurement;
+
+import java.io.*;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
+    private final String fileName;
+
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
-    public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
+    public List<Measurement> load() throws IOException {
+        try (var bufferedReader = new BufferedReader(new FileReader(this.fileName))){
+            System.out.println("text from the file:");
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+             //читает файл, парсит и возвращает результат
         return null;
+    }
+
+    public static void main(String[] args){
+        var inputDataFileName = "inputData.json";
+
+        ResourcesFileLoader resourcesFileLoader = new ResourcesFileLoader(inputDataFileName);
+        try {
+            resourcesFileLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
