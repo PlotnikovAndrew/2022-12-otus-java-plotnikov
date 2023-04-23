ed46d87ce3ee84b524026a237bdd3e18b4044772
PlotnikovAndrew <drunyy@gmail.com>
2023-04-23 13:48:26 +0300

    completed hw21-jpql

--- hw21-jpql/src/main/java/ru/otus/crm/model/Client.java
+++ hw21-jpql/src/main/java/ru/otus/crm/model/Client.java
@@ -1,37 +1,49 @@
 package ru.otus.crm.model;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-@Table(name = "client")
+@Table(name = "clients")
 public class Client implements Cloneable {
 
     @Id
     @SequenceGenerator(name = "client_gen", sequenceName = "client_seq",
             initialValue = 1, allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_gen")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id")
     private Long id;
 
     @Column(name = "name")
     private String name;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true, mappedBy = "client")
+    private List<Phone> phoneList;
+
     public Client(String name) {
         this.id = null;
         this.name = name;
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phoneList){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phoneList = phoneList;
+        for (Phone phone : this.phoneList){
+            phone.setClient(this);
+        }
     }
 
     public Client(Long id, String name) {
@@ -41,7 +53,10 @@ public class Client implements Cloneable {
 
     @Override
     public Client clone() {
-        return new Client(this.id, this.name);
+        Client newClient = new Client(this.id, this.name);
+        newClient.setAddress(new Address(this.address.getId(), this.address.getStreet()));
+        newClient.setPhoneList(this.getPhoneList().stream().map(oldPhoneList -> new Phone(oldPhoneList.getId(), oldPhoneList.getPhoneNumber(), newClient)).toList());
+        return newClient;
     }
 
     @Override
